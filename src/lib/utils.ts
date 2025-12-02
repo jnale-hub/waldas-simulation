c2f433ad92af6be51f8b4be397e52ba6c9733317
John Mark Delima <delimajohnmark14@gmail.com>
2025-12-02 17:55:19 +0800

    Add utility functions for formatting money and numbers

---
+++ src/lib/utils.ts
@@ -0,0 +1,11 @@
+export const formatMoney = (amount: number): string => {
+  return new Intl.NumberFormat('en-PH', {
+    style: 'currency',
+    currency: 'PHP',
+    maximumFractionDigits: 0,
+  }).format(amount);
+};
+
+export const formatNumber = (num: number): string => {
+  return new Intl.NumberFormat('en-US').format(num);
+};
